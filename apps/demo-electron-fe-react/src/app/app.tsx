a0d100c53a89c03a5f56f88f9508e8ab700f3c26
hoang1703 <gvscotcot@gmail.com>
2023-04-10 18:01:05 +0700

    demo electron and nextjs

---
+++ apps/demo-electron-fe-react/src/app/app.tsx
@@ -0,0 +1,12 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="demo-electron-fe-react" />
+      <div />
+    </>
+  );
+}
+
+export default App;
---
+++ apps/electron/demo-electron/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'demo-electron-fe-react'; // options.name.split('-')[0] + '-web'
+export const electronAppName = 'electron-demo-electron';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
